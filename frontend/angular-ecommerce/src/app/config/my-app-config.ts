b2cb9834a9dded3485d48ca2d8ee812d9460f2a8
Marcin Głąb <marcinglabppx@gmail.com>
2024-11-05 14:13:03 +0100

    Aktualizacja

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oaktzndpuN4yWkWe5d7',
+        issuer: 'https://dev-79784078.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
